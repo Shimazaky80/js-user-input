84224575d682a165aa53c869e85edec7c1c6ec3b
Shimazaky80 <64324142+Shimazaky80@users.noreply.github.com>
2023-09-10 14:33:08 +0200

    Fixed textbox user input

--- with-textbox.js
+++ with-textbox.js
@@ -1,9 +1,9 @@
-let labelName = document.getElementById("labelName")
-let userName = document.getElementById("userName")
 let submitBtn = document.getElementById("submitBtn")
 
 submitBtn.onclick = () => {
 
-    labelName.innerHTML = `You entered: ${userName.value}`
+    let labelName = document.getElementById("labelName")
+    let userName = document.getElementById("userName")
+    labelName.innerHTML = `Welcome ${userName.value}!`
 
 }
